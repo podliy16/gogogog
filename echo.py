3a7c22f755c64dd02869897f3488802400a7b8d2
VanAbsenter <van.absenter@gmail.com>
2015-10-19 21:31:03 +0500

    19.10.2015

---
+++ echo.py
@@ -0,0 +1,18 @@
+import socket, os, time, sys
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+port = int(sys.argv[1])
+s.bind(("localhost", port))
+print(port)
+s.listen(1)
+while 1:
+	conn, addr = s.accept()
+	print(conn, addr)
+	while 1:
+		data = conn.recv(1024)
+		if len(data)==0:
+			break
+		time.sleep(5)
+		conn.send(data)
+conn.close()
