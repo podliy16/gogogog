b07bc4fd7f25f747d293ef14d81b4567a59b97f5
VanAbsenter <van.absenter@gmail.com>
2015-10-19 21:34:55 +0500

    qwerty

--- echo.py
+++ echo.py
@@ -10,11 +10,12 @@ while 1:
 	conn, addr = s.accept()
 	print(conn, addr)
 	while 1:
-		data = conn.recv(1024)
-		if len(data)==0:
+		try:
+			data = conn.recv(1024)
+			time.sleep(5)
+			conn.send(data)
+		except:
+			print('PANIC')
 			break
-		time.sleep(5)
-		conn.send(data)
-		
 
 conn.close()
